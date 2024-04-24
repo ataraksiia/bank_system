3d4ac9c7d70801256dcab99542bdbae45d60bbd3
Ataraksia <1509lisavi@gmail.com>
2024-04-24 22:56:53 +0300

    Add generators, test_generators

--- src/generators.py
+++ src/generators.py
@@ -5,7 +5,7 @@ def filter_by_currency(transaction_array: list, currency: str) -> Iterator:
     """Функция, которая принимает список словарей  и возвращает итератор, который по очеред выдает операции,
     в которых указана заданная валюта."""
 
-    # Итерируемся по полученному списку и сравниваем валюты. Если одиннаковые, то возвращаем итератор.
+    # Итерируемся по полученному списку и сравниваем валюты. Если одинаковые, то возвращаем итератор.
     for operation in transaction_array:
         if operation["operationAmount"]["currency"]["code"] == currency:
             yield operation
